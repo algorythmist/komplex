88cb717dc8345394da093fc458e2ba76850fbe1f
dimitri <dimitri@trizic.com>
2018-05-27 19:02:41 -0700

    addition

--- src/main/kotlin/Complex.kt
+++ src/main/kotlin/Complex.kt
@@ -1,14 +1,16 @@
 package complex
 
-import kotlin.math.abs
-
 class Complex(val real: Double, val img: Double) {
+
+    operator fun unaryMinus() = Complex(-real, -img)
+
+    operator fun plus(c: Complex) = Complex(real + c.real, img + c.img)
 
     override fun toString(): String {
         if (img < 0) {
-            return "" + real +"-" + abs(img)+"i"
+            return "%.4f - %.4fi".format(real, -img)
         }
-        return "" + real + "+" + img + "i"
+        return "%.4f + %.4fi".format(real, img)
     }
 
 }
--- src/test/kotlin/ComplexTest.kt
+++ src/test/kotlin/ComplexTest.kt
@@ -8,7 +8,15 @@ class ComplexTest {
 
     @Test
     fun testComplex() {
-        val c1  = Complex(2.0, -3.0)
-        Assertions.assertEquals("2.0-3.0i", c1.toString())
+        val c1  = Complex(2.0, -3.1)
+        Assertions.assertEquals("2.0000 - 3.1000i", c1.toString())
+        val c2 = -c1;
+        Assertions.assertEquals("-2.0000 + 3.1000i", c2.toString())
+
+        val c3 = Complex(-5.0, 2.0)
+
+        val c4 = c1+c3
+        Assertions.assertEquals(-3.0, c4.real)
+        Assertions.assertEquals(-1.1, c4.img)
     }
 }
