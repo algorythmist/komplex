a99d6627fb16ae810c71c58f102a3caa5742d544
dimitri <dimitri@trizic.com>
2018-05-28 08:33:28 -0700

    multiplication and division

--- src/main/kotlin/Complex.kt
+++ src/main/kotlin/Complex.kt
@@ -1,10 +1,28 @@
 package complex
 
+
+
 class Complex(val real: Double, val img: Double) {
 
     operator fun unaryMinus() = Complex(-real, -img)
 
     operator fun plus(c: Complex) = Complex(real + c.real, img + c.img)
+
+    operator fun times(c: Complex) = Complex(real*c.real - img*c.img, real*c.img+ img*c.real)
+
+    operator fun times(n: Number) = Complex(n.toDouble()*real, n.toDouble()*img)
+
+    operator fun div(n: Number) = Complex(real/n.toDouble(), img/n.toDouble())
+
+    operator fun div(c: Complex) : Complex {
+        val den = c.normSquared();
+        val num = this*c.conjugate()
+        return num/den
+    }
+
+    fun conjugate(): Complex = Complex(real ,-img)
+
+    fun normSquared() : Double = real*real + img*img
 
     override fun toString(): String {
         if (img < 0) {
--- src/test/kotlin/ComplexTest.kt
+++ src/test/kotlin/ComplexTest.kt
@@ -15,8 +15,51 @@ class ComplexTest {
 
         val c3 = Complex(-5.0, 2.0)
 
-        val c4 = c1+c3
+        var c4 = c1+c3
         Assertions.assertEquals(-3.0, c4.real)
         Assertions.assertEquals(-1.1, c4.img)
+
+        c4 =  c1*c3
+        Assertions.assertEquals(-3.8, c4.real)
+        Assertions.assertEquals(19.5, c4.img)
+
+    }
+
+    @Test
+    fun testMultiplication() {
+        val c1  = Complex(2.0, -3.1)
+
+        var c2 = c1*2
+        Assertions.assertEquals(4.0, c2.real)
+        Assertions.assertEquals(-6.2, c2.img)
+
+        c2 = c1*2.5
+        Assertions.assertEquals(5.0, c2.real)
+        Assertions.assertEquals(-7.75, c2.img)
+
+        c2 = c1*0
+        Assertions.assertEquals(0.0, c2.real)
+        Assertions.assertEquals(0.0, c2.img, 0.001)
+    }
+
+    @Test
+    fun testDivision() {
+        val c1  = Complex(3.0, -2.5)
+        var c2 = c1 / 2.0
+        Assertions.assertEquals(1.5, c2.real)
+        Assertions.assertEquals(-1.25, c2.img)
+
+        c2 = c1 / Complex(-1.0, 1.0)
+        Assertions.assertEquals(-2.75, c2.real)
+        Assertions.assertEquals(-0.25, c2.img)
+
+        println(c2*Complex(-1.0, 1.0))
+    }
+
+    @Test
+    fun testDivideByZero() {
+        val c1  = Complex(3.0, -2.5)
+        val c2 = c1/0
+        println(c2)
     }
 }
