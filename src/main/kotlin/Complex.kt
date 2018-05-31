4112655d2890d69c91a38b74cb8336f30e594e02
dimitri <dimitri@trizic.com>
2018-05-31 08:47:44 -0700

    some special functions

--- src/main/kotlin/Complex.kt
+++ src/main/kotlin/Complex.kt
@@ -4,7 +4,8 @@ import kotlin.math.cos
 import kotlin.math.exp
 import kotlin.math.sin
 import kotlin.math.sqrt
-
+import kotlin.math.atan
+import kotlin.math.ln
 
 fun abs(c: Complex): Double = c.abs()
 
@@ -13,14 +14,39 @@ fun exp(c: Complex): Complex {
     return Complex(e * cos(c.img), e * sin(c.img))
 }
 
+fun sinh(c: Complex) = (exp(c) - exp(-c))/2
+
+fun cosh(c: Complex) = (exp(c) + exp(-c))/2
+
+fun tanh(c: Complex) = sinh(c) / cosh(c)
+
+fun coth(c: Complex) =  cosh(c) / sinh(c)
+
+/**
+ * The natural logarithm on the principal branch
+ */
+fun ln(c: Complex) = Complex(ln(c.abs()), c.phase())
+
 /**
  * Defines complex numbers and their algebraic operations
  */
 class Complex(val real: Double, val img: Double) {
 
+    override fun equals(other: Any?): Boolean {
+        if (other == null || other !is Complex || real != other.real || img != other.img)
+            return false
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return real.hashCode() * 31 + img.hashCode()
+    }
+
     operator fun unaryMinus() = Complex(-real, -img)
 
     operator fun plus(c: Complex) = Complex(real + c.real, img + c.img)
+
+    operator fun minus(c: Complex) = Complex(real - c.real, img - c.img)
 
     operator fun times(c: Complex) = Complex(real * c.real - img * c.img, real * c.img + img * c.real)
 
@@ -39,6 +65,8 @@ class Complex(val real: Double, val img: Double) {
     fun normSquared(): Double = real * real + img * img
 
     fun abs(): Double = sqrt(this.normSquared())
+
+    fun phase(): Double = atan(img/real)
 
     override fun toString(): String {
         if (img < 0) {
--- src/test/kotlin/ComplexTest.kt
+++ src/test/kotlin/ComplexTest.kt
@@ -1,20 +1,30 @@
 package complex
 
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import kotlin.math.PI
 
 class ComplexTest {
 
+
     @Test
-    fun testComplex() {
+    fun testEquals() {
+        val c1  = Complex(2.0, -3.1)
+        var c2  = Complex(2.0, -3.1)
+        assertEquals(c1, c2)
+        assertTrue(c1 == c2)
+    }
+
+    @Test
+    fun testAdditionSubtraction() {
         val c1  = Complex(2.0, -3.1)
         Assertions.assertEquals("2.0000 - 3.1000i", c1.toString())
         val c2 = -c1
         Assertions.assertEquals("-2.0000 + 3.1000i", c2.toString())
 
         val c3 = Complex(-5.0, 2.0)
-
         var c4 = c1+c3
         Assertions.assertEquals(-3.0, c4.real)
         Assertions.assertEquals(-1.1, c4.img)
@@ -52,29 +62,50 @@ class ComplexTest {
         c2 = c1 / Complex(-1.0, 1.0)
         Assertions.assertEquals(-2.75, c2.real)
         Assertions.assertEquals(-0.25, c2.img)
-
-        println(c2*Complex(-1.0, 1.0))
     }
 
     @Test
     fun testDivideByZero() {
         val c1  = Complex(3.0, -2.5)
         val c2 = c1/0
-        println(c2)
+        println(c2) //TODO
     }
 
     @Test
-    fun testNorm() {
+    fun testPolarCoordinates() {
         val c1  = Complex(3.0, -4.0)
         Assertions.assertEquals(5.0, c1.abs())
         Assertions.assertEquals(5.0, abs(c1))
+        Assertions.assertEquals(-0.9273, c1.phase(), 0.001)
     }
 
     @Test
-    fun testExp() {
+    fun testSpecialFunctions() {
         val c1 = Complex(0.0, PI)
         val c2 = exp(c1)
         Assertions.assertEquals("-1.0000 + 0.0000i", c2.toString())
+
+        val c3 = Complex(2.0, -1.0)
+        var z = exp(c3)
+        Assertions.assertEquals(3.99232, z.real, 0.0001)
+        Assertions.assertEquals(-6.217676, z.img, 0.0001)
+
+        z = sinh(c3)
+        Assertions.assertEquals(1.95960, z.real, 0.0001)
+        Assertions.assertEquals(-3.165778, z.img, 0.0001)
+
+        z = cosh(c3)
+        Assertions.assertEquals(2.0327230, z.real, 0.0001)
+        Assertions.assertEquals(-3.0518977, z.img, 0.0001)
+
+        z = tanh(c3)
+        Assertions.assertEquals(1.014793, z.real, 0.0001)
+        Assertions.assertEquals(-0.0338128, z.img, 0.0001)
+
+        z = coth(c3)
+        Assertions.assertEquals(0.98432922, z.real, 0.0001)
+        Assertions.assertEquals(0.0327977, z.img, 0.0001)
+
     }
 
 }
